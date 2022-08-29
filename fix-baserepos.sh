68544f425c0496020284611b0953c9f8b59a0280
plusforum <103184903+plusforum@users.noreply.github.com>
2022-08-29 09:45:32 +0530

    Update fix-baserepos.sh

--- fix-baserepos.sh
+++ fix-baserepos.sh
@@ -1,3 +1,5 @@
+### Update Base repo using below set of commands.
+
 dnf clean all
 
 rm -rf /var/cache/dnf
@@ -7,3 +9,14 @@ cd /etc/yum.repos.d/
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
+sudo yum update
+
+### Install Docker-ce using below set of commands
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum autoremove runc -y
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
